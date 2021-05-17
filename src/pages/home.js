16e1b7d4a77c9c0728451a5731024bd05356bc96
angel_anna.matthew <angel_anna.mathew@bell.ca>
2021-05-17 17:04:53 -0400

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Info from "../components/Info/Info";
+import Contact from "../components/Contact/Contact";
+const Home=()=>{
+    return(<>
+        <Header/>
+     
+       <Info/>
+       <Contact/>
+       </>
+    )
+}
+
+export default Home;
---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { Router } from "@reach/router";
+import Home from "../pages/home";
+import Header from "../components/Header/Header";
+import Info from "../components/Info/Info";
+import Contact from "../components/Contact/Contact";
+// markup
+const IndexPage = () => {
+  return (
+    
+    <main>
+      
+      <title>Home Page</title>
+    
+      {/* <Header/> */}
+      {/* <Router> */}
+    <Home/>
+     {/* </Router> */}
+    
+   
+      {/* <Link to="/home/">Contact</Link> */}
+
+     
+      
+    </main>
+  )
+}
+
+export default IndexPage
